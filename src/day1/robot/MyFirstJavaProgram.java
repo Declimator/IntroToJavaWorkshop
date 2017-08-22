b3280be9908086b1bbedd90ea188fc61d725a170
Declimator <31232768+Declimator@users.noreply.github.com>
2017-08-22 11:56:16 -0700

    Day 2 Workshop

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -1,15 +1,83 @@
 package day1.robot;
 
 import java.awt.Color;
+
+import org.jointheleague.graphical.robot.KeyboardAdapter;
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
+	private static KeyboardAdapter adapter;
+
 	public static void main(String[] args) {
 		
 		// START HERE
-
-	
-		
-		
+System.out.println("Hello World");
+Robot o =new Robot("mini");
+	o.penDown();
+	o.setRandomPenColor();
+	o.setPenWidth(100);
+	o.setSpeed(100);
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.turn(90);
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.turn(90);
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.setRandomPenColor();	
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.turn(90);
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);
+	o.setRandomPenColor();
+	o.move(25);	
+	o.setRandomPenColor();
+	o.move(25);
 	}
 }
--- src/day1/robot/TriangleShell.java
+++ src/day1/robot/TriangleShell.java
@@ -5,29 +5,219 @@ import org.jointheleague.graphical.robot.Robot;
 public class TriangleShell {
 
 	// 1. Create a new Robot
-
+Robot o = new Robot("mini");
 	
 	void go() {
-		drawTriangle(100); // 3. delete this line (used only for testing)
+// 3. delete this line (used only for testing)
 
 		// 6. Make the robot go as fast as possible
-
+o.setSpeed(10);
 		// 4. make a variable to hold the length of the triangle and set it to 50
-
+int length = 50 ;
 		// 7. Do the following (up to step 10) 60 times
-
+for( int i =0 ; i < 60; length += 5) {
 			// 9. Change the color of the pen to a random color
 	
 			// 8. Increase the length of the side by 10 pixels
-	
-			// 5. call your drawTriangle() method using your length variable
-	
-			// 10. Turn the tortoise 6 degrees to the right
 
+			// 5. call your drawTriangle() method using your length variable
+	drawTriangle(length);
+			// 10. Turn the tortoise 6 degrees to the right
+o.turn(6);
+}
 	}
 
 	/* 2. fill in the method below to draw a triangle. Use the length variable for the size of the triangle. */
 	private void drawTriangle(int length) {
+		o.penDown();
+
+		o.setSpeed(100);
+		//turn
+		o.turn(30);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		//turn
+		o.turn(120);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		//turn
+		o.turn(120);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
+		o.setRandomPenColor();
+		o.setSpeed(100);
+		o.move(length/20);
 		
 		
 		
